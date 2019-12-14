98f9345da18c438ee944d8ea8c6c019e34e29041
King-Coyote <alex.hartstone@gmail.com>
2019-12-15 03:12:05 +1100

    24: Allowed stackops to get the number of items on stack

---
+++ src/tests/StackOpsTests.cpp
@@ -0,0 +1,32 @@
+#include "catch.hpp"
+#include "mun/State.hpp"
+#include "mun/StackOps.hpp"
+
+#include <memory>
+#include <iostream>
+
+using namespace std;
+using namespace mun;
+
+SCENARIO("mun::StackOps functions work as expected", "[stackops]") {
+
+    GIVEN("A mun::Function object") {
+
+        auto p_State = unique_ptr<State>(new State());
+        auto L = p_State->getStatePointer();
+        p_State->openLibs();
+
+        WHEN("Items have been placed on the stack") {
+            THEN("The stack count is correct") {
+                pushValue(L, 10);
+                pushValue(L, 10);
+                pushValue(L, 10);
+                CHECK(countStack(L) == 3);
+                lua_pop(L, -1);
+                CHECK(countStack(L) == 0);
+            }
+        }
+
+    }
+
+}
